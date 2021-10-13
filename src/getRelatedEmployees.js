1ac60062cd4975bff08d293c66de1c8857dc94ca
Emerson <eemr3@yahoo.com.br>
2021-10-13 15:03:32 -0300

    Implementa função getRelatedEmployees - requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((managerId) => managerId.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error(
+      'O id inserido não é de uma pessoa colaboradora gerente!',
+    );
+  }
+
+  return employees.reduce((acc, employee) => {
+    if (employee.managers.includes(managerId)) {
+      acc.push(`${employee.firstName} ${employee.lastName}`);
+    }
+    return acc;
+  }, []);
 }
 
 module.exports = { isManager, getRelatedEmployees };
