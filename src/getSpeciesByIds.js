ffbb7790b9a4f8b96b3708c73b4eef8158f4ae08
Emerson <eemr3@yahoo.com.br>
2021-10-13 14:36:47 -0300

    Implementa função getSpeciesById - requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) =>
+    data.species.reduce((acc, curr) => {
+      if (curr.id === id) return curr;
+      return acc;
+    }));
 }
 
 module.exports = getSpeciesByIds;
