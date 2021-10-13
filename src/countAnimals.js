7c67550254dc055c3d1198ac451c1581acfdb6a9
Emerson <eemr3@yahoo.com.br>
2021-10-13 15:06:43 -0300

    Implementa função countAnimals - requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function countResidents(parmas) {
+  const residents = species.reduce((acc, specie) =>
+    (specie.name === parmas.specie ? specie.residents : acc));
+  if (!parmas.sex) return residents.length;
+
+  return residents.reduce(
+    (acc, currentSpecie) => (currentSpecie.sex === parmas.sex ? acc + 1 : acc),
+    0,
+  );
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  return countResidents(animal);
 }
 
 module.exports = countAnimals;
