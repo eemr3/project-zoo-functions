a3d0d998ce04a90c2a53755ccf8f75b20f7a44cb
Emerson <eemr3@yahoo.com.br>
2021-10-14 14:00:10 -0300

    Remove linha de código inutil

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,15 +1,6 @@
 const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  // { name: 'Pedro Henrique Carvalho', age: 5 },
-  // { name: 'Maria Costa', age: 18 },
-  // { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
 function countEntrants(entrant) {
   let countChild = 0;
   let countAdult = 0;
@@ -41,6 +32,5 @@ function calculateEntry(entrant) {
 
   return total;
 }
-console.log(calculateEntry(entrants));
-console.log(countEntrants(entrants));
+
 module.exports = { calculateEntry, countEntrants };
