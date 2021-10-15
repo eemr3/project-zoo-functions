0ce32206e5f1c54f305a98d603a8359211fca0af
Emerson <eemr3@yahoo.com.br>
2021-10-15 19:06:43 -0300

    Implementa função getOldestFromFirstSpecies - requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idRsponsibleEmployee = employees.find(
+    (employee) => employee.id === id,
+  ).responsibleFor;
+
+  return species.reduce((acc, currentSpecie) => {
+    if (idRsponsibleEmployee[0] === currentSpecie.id) {
+      return Object.values(currentSpecie.residents.reduce((accResidente, resident) =>
+        (accResidente.age > resident.age ? accResidente : resident)));
+    }
+
+    return acc;
+  }, 0);
 }
 
 module.exports = getOldestFromFirstSpecies;
