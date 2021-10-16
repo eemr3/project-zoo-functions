81ac37dc006e0fe4d165d7845317eea8db324499
Emerson <eemr3@yahoo.com.br>
2021-10-15 22:16:52 -0300

    Implementa função getEmployeesCoverage - requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,51 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const getEmployeesCoverageNotParams = () => employees.map((employee) => ({
+  id: employee.id,
+  fullName: `${employee.firstName} ${employee.lastName}`,
+  species: species.reduce((acc, specie) => (employee.responsibleFor.includes(specie.id)
+    ? acc.concat(specie.name) : acc), []),
+  locations: species.reduce((accLocation, location) => (
+    employee.responsibleFor.includes(location.id)
+      ? accLocation.concat(location.location) : accLocation), []),
+}));
+
+const isEmployeesCoverage = ({ id, name }) => employees.some((employee) =>
+  employee.id === id || employee.firstName === name || employee.lastName === name);
+
+const getEmployeeParameter = ({ id, name }) => {
+  if (!isEmployeesCoverage({ id, name })) {
+    throw new Error('Informações inválidas');
+  }
+  return employees.filter(
+    (employee) =>
+      employee.id === id
+      || employee.firstName === name
+      || employee.lastName === name,
+  );
+};
+
+const testeReduce = (employee) => {
+  const result = getEmployeeParameter(employee);
+  return result.reduce((acc, current) => ({
+    id: current.id,
+    fullName: `${current.firstName} ${current.lastName}`,
+    species: species.reduce(
+      (accSp, specie) => (current.responsibleFor.includes(specie.id)
+        ? accSp.concat(specie.name)
+        : accSp),
+      [],
+    ),
+    locations: species.reduce((accLoc, location) => (current.responsibleFor.includes(location.id)
+      ? accLoc.concat(location.location)
+      : accLoc),
+    []),
+  }), {});
+};
+
+function getEmployeesCoverage(employee) {
+  if (!employee) return getEmployeesCoverageNotParams();
+  return testeReduce(employee);
 }
 
-module.exports = getEmployeesCoverage;
+(module.exports = getEmployeesCoverage);
