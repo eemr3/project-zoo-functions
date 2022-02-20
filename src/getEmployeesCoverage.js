9ffc0cbeea09f13bf9fff74214f8408b7d92aa94
Emerson <eemr3@yahoo.com.br>
2022-02-19 21:06:57 -0300

    refactor: getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,48 +1,68 @@
-const { employees, species } = require('../data/zoo_data');
+const { employees, species } = require("../data/zoo_data");
 
-const getEmployeesCoverageNotParams = () => employees.map((employee) => ({
-  id: employee.id,
-  fullName: `${employee.firstName} ${employee.lastName}`,
-  species: species.reduce((acc, specie) => (employee.responsibleFor.includes(specie.id)
-    ? acc.concat(specie.name) : acc), []),
-  locations: species.reduce((accLocation, location) => (
-    employee.responsibleFor.includes(location.id)
-      ? accLocation.concat(location.location) : accLocation), []),
-}));
+const getEmployeesCoverageNotParams = () =>
+  employees.map((employee) => ({
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: species.reduce(
+      (acc, specie) =>
+        employee.responsibleFor.includes(specie.id)
+          ? acc.concat(specie.name)
+          : acc,
+      []
+    ),
+    locations: species.reduce(
+      (accLocation, location) =>
+        employee.responsibleFor.includes(location.id)
+          ? accLocation.concat(location.location)
+          : accLocation,
+      []
+    ),
+  }));
 
-const isEmployeesCoverage = ({ id, name }) => employees.some((employee) =>
-  employee.id === id || employee.firstName === name || employee.lastName === name);
+const isEmployeesCoverage = ({ id, name }) =>
+  employees.some(
+    (employee) =>
+      employee.id === id ||
+      employee.firstName === name ||
+      employee.lastName === name
+  );
 
 const getEmployeeParameter = ({ id, name }) => {
   if (!isEmployeesCoverage({ id, name })) {
-    throw new Error('Informações inválidas');
+    throw new Error("Informações inválidas");
   }
   return employees.filter(
     (employee) =>
-      employee.id === id
-      || employee.firstName === name
-      || employee.lastName === name,
+      employee.id === id ||
+      employee.firstName === name ||
+      employee.lastName === name
   );
 };
 
 const getEmployeesWithParameter = (employee) => {
   const result = getEmployeeParameter(employee);
-  return result.reduce((acc, current) => ({ id: current.id,
-    fullName: `${current.firstName} ${current.lastName}`,
-    species: species.reduce((accSp, specie) =>
-      (current.responsibleFor.includes(specie.id)
-        ? accSp.concat(specie.name)
-        : accSp),
-    []),
-    locations: species.reduce(
-      (accLoc, location) =>
-        (current.responsibleFor.includes(location.id)
-          ? accLoc.concat(location.location)
-          : accLoc),
-      [],
-    ),
-  }),
-  {});
+  return result.reduce(
+    (acc, current) => ({
+      id: current.id,
+      fullName: `${current.firstName} ${current.lastName}`,
+      species: species.reduce(
+        (accSp, specie) =>
+          current.responsibleFor.includes(specie.id)
+            ? accSp.concat(specie.name)
+            : accSp,
+        []
+      ),
+      locations: species.reduce(
+        (accLoc, location) =>
+          current.responsibleFor.includes(location.id)
+            ? accLoc.concat(location.location)
+            : accLoc,
+        []
+      ),
+    }),
+    {}
+  );
 };
 
 function getEmployeesCoverage(employee) {
@@ -50,4 +70,4 @@ function getEmployeesCoverage(employee) {
   return getEmployeesWithParameter(employee);
 }
 
-(module.exports = getEmployeesCoverage);
+module.exports = getEmployeesCoverage;
